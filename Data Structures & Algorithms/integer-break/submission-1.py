8feebd7c78c2046367b0966837c25b881366e160
Mouad Ouchelh <mouadbusiness05@proton.me>
2026-07-01 20:34:29 +0100

    Add: integer-break - submission-1

---
+++ Data Structures & Algorithms/integer-break/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        memo={}
+        def dfs(num):
+            if num in memo : 
+                return memo[num]
+            if num == 1:
+                return 1
+            res = 0 if num == n else num
+            for i in range(1, num):
+                val = dfs(i) * dfs(num - i)
+                res = max(res, val)
+            memo[num]=res
+            return res
+        return dfs(n)
