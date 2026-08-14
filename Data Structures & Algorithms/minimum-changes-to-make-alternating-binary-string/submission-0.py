d96fc63d467d8147b5dc6b2f07962e246d3dfa61
Mouad Ouchelh <mouadbusiness05@proton.me>
2026-08-14 08:54:37 +0100

    Add: minimum-changes-to-make-alternating-binary-string - submission-0

---
+++ Data Structures & Algorithms/minimum-changes-to-make-alternating-binary-string/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minOperations(self, s: str) -> int:
+        first=0
+        for i in range(len(s)):
+            if i%2==0 and s[i]!='0':
+                first+=1
+            elif i%2!=0 and s[i]!='1':
+                first+=1
+        second=0
+        for i in range(len(s)):
+            if i%2==0 and s[i]=='0':
+                second+=1
+            elif i%2!=0 and s[i]=='1':
+                second+=1
+        return min(first,second)
